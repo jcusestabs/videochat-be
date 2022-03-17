8faee0fdc63be059acc0c65514cd0f4c90b79f38
JC Ramirez <jcramirez@applaudostudios.com>
2022-03-16 23:00:00 -0600

    :poop: change detail on env

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: process.env["HOST"],
+    origin: process.env.HOST,
     methods: ["GET", "POST"],
   },
 });
